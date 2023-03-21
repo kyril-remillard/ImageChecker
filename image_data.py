f01f290a8b78326bbcded69e997d9c4617287951
kyril-remillard <kyril.remillard@moveworks.ai>
2023-03-21 15:26:36 -0400

    remove old files

--- image_data.py
+++
@@ -1,2 +0,0 @@
-# this holds all the data for every image that is successfully reached by the server
-labelbox_image_data = {}
--- image_queue.py
+++
@@ -1,9 +0,0 @@
-from image_validator import validate_image
-from image_data import labelbox_image_data
-from helper_functions import set_validation_state
-
-
-labelbox_image_queue = []
-
-def queue_image(image_data):
-  validate_image(image_data)
